deee787b25d2d5fb8fa3da172a27661e520437f3
Alex Poon <chpoon92@gmail.com>
2015-02-17 01:45:45 +0800

    Add lab 3

---
+++ lab_3/README.md
@@ -0,0 +1,14 @@
+Requirements 
+
+You may notice that in the sample today, the stopword list is hard-coded by
+inserting each word line by line. You are required to modify the example program
+to read stopwords from a file, stopwords.txt.
+
+You only need to modify the code in "StopStem" function, which is used to read
+the stopwords from txt file into memory. You are not required to touch other
+code in sample program. Thus, a package named as "IRUtilities" should be created
+under project and Porter.java should be put under the package.
+
+According to the sample code, "StopWords.txt" should be set the same directory
+as "src" folder of the project. Learn by yourself about how to read external txt
+file by "BufferedReader".
---
+++ lab_3/src/main/java/hk/ust/comp4321/lab/StopStem.java
@@ -0,0 +1,62 @@
+package hk.ust.comp4321.lab;
+
+import IRUtilities.*;
+import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class StopStem {
+  private Porter porter;
+  private java.util.HashSet stopWords;
+
+  public boolean isStopWord(String str) {
+    return stopWords.contains(str);  
+  }
+
+  public StopStem(String str) {
+    super();
+    porter = new Porter();
+    stopWords = new java.util.HashSet();
+    try {  
+      BufferedReader in = new BufferedReader(new FileReader(str));
+      String line = in.readLine();
+      while (line != null) {
+        stopWords.add(line);
+        line = in.readLine();
+      }
+      in.close();
+    } catch (IOException e) {
+      System.err.println("Error!");
+    }
+  }
+
+  public String stem(String str) {
+    return porter.stripAffixes(str);
+  }
+
+  public static void main(String[] arg) {
+    StopStem stopStem = new StopStem("stopwords.txt");
+    String input="";
+    try {
+      do {
+        System.out.print("Please enter a single English word: ");
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(System.in));
+        input = in.readLine();
+        if (input.length()>0) {  
+          if (stopStem.isStopWord(input))
+            System.out.println("It should be stopped");
+          else
+            System.out.println("The stem of it is \"" + stopStem.stem(input)
+                + "\"");
+        }
+      } while (input.length()>0);
+    } catch(IOException ioe) {
+      System.err.println(ioe.toString());
+    }
+  }
+}
